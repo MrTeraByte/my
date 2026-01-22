659fdc27bd4f661b1db87f217dc6d36bf94d4f90
saz <92567869+MrTeraByte@users.noreply.github.com>
2026-01-22 17:53:27 +0000

    more secret varable fixes

--- transfer.ts
+++ transfer.ts
@@ -6,6 +6,7 @@ import axios from "axios";
 const r2Client = new S3Client({
   region: "auto",
   endpoint: process.env.R2_ENDPOINT,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
